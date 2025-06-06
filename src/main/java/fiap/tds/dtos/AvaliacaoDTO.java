0fc35db0de933e3cccfae0ac7a36e098026f691c
joaoGFG <joaogabrielfg10@gmail.com>
2025-06-06 17:40:45 -0300

    feat: getters e setters avaliacaoDTO

--- src/main/java/fiap/tds/dtos/AvaliacaoDTO.java
+++ src/main/java/fiap/tds/dtos/AvaliacaoDTO.java
@@ -1,12 +1,53 @@
 package fiap.tds.dtos;
 
-import fiap.tds.model.NivelRisco;
 import fiap.tds.model.TipoConstrucao;
 
 public class AvaliacaoDTO {
-    public int usuarioId;
-    public boolean moraEmEncosta;
-    public boolean ruaAlaga;
-    public TipoConstrucao tipoConstrucao;
-    public int numeroPessoas;
+    private int usuarioId;
+    private boolean moraEmEncosta;
+    private boolean ruaAlaga;
+    private TipoConstrucao tipoConstrucao;
+    private int numeroPessoas;
+
+    public AvaliacaoDTO() {}
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public boolean isMoraEmEncosta() {
+        return moraEmEncosta;
+    }
+
+    public void setMoraEmEncosta(boolean moraEmEncosta) {
+        this.moraEmEncosta = moraEmEncosta;
+    }
+
+    public boolean isRuaAlaga() {
+        return ruaAlaga;
+    }
+
+    public void setRuaAlaga(boolean ruaAlaga) {
+        this.ruaAlaga = ruaAlaga;
+    }
+
+    public TipoConstrucao getTipoConstrucao() {
+        return tipoConstrucao;
+    }
+
+    public void setTipoConstrucao(TipoConstrucao tipoConstrucao) {
+        this.tipoConstrucao = tipoConstrucao;
+    }
+
+    public int getNumeroPessoas() {
+        return numeroPessoas;
+    }
+
+    public void setNumeroPessoas(int numeroPessoas) {
+        this.numeroPessoas = numeroPessoas;
+    }
 }
