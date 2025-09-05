0ebde37bafea57fd002c5089fbbf2402a3c535b5
joaoGFG <joaogabrielfg10@gmail.com>
2025-09-05 13:33:41 -0300

    fix: refactoring dbconfig

--- src/main/java/fiap/tds/infrastructure/DatabaseConfig.java
+++ src/main/java/fiap/tds/infrastructure/DatabaseConfig.java
@@ -5,22 +5,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String HOST = "db.fggbbvlmhltcswxadefm.supabase.co"; // Cole o Host aqui
-    private static final String DATABASE = "postgres"; // Geralmente é 'postgres'
-    private static final String PORT = "5432"; // Cole a Porta aqui
-    private static final String USER = "postgres"; // Cole o User aqui
-    private static final String PASSWORD = "qGRXOeIaiOvELJZv"; // Cole a Senha aqui
+    private static final String HOST = "db.fggbbvlmhltcswxadefm.supabase.co"; 
+    private static final String DATABASE = "postgres"; 
+    private static final String PORT = "5432"; 
+    private static final String USER = "postgres"; 
+    private static final String PASSWORD = "qGRXOeIaiOvELJZv"; 
 
-    // URL de conexão para PostgreSQL
-    private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE;
+    // URL de conexão para PostgreSQL com SSL obrigatório
+    private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE + "?sslmode=require";
 
     public static Connection getConnection() throws SQLException {
         try {
-            // Garante que o driver do PostgreSQL foi carregado
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
-            // Este erro acontece se o .jar do driver não estiver no projeto
             throw new SQLException("Driver do PostgreSQL não encontrado!", e);
         }
     }
